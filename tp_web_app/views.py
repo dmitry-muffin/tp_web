bb2bfacdb8ca4c786f5c6b055ca3e9cc5cd2a072
dmitry_muffin <andima70@gmail.com>
2025-03-16 18:18:51 +0300

    HW_1

--- tp_web_app/views.py
+++ tp_web_app/views.py
@@ -9,5 +9,26 @@ def index(request):
 def index_na(request):
     return render(request, 'index_na.html')
 
+
 def ask(request):
     return render(request, 'ask.html')
+
+
+def question(request):
+    return render(request, 'question.html')
+
+
+def tag(request):
+    return render(request, 'tag.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
--- tp_web_proj/urls.py
+++ tp_web_proj/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('index_na', index_na, name='index_na'),
-    path('ask', ask, name='ask')
+    path('ask', ask, name='ask'),
+    path('question', question, name='question'),
+    path('tag', tag, name='tag'),
+    path('settings', settings, name='settings'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
 ]
